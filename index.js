17e71f2b5fb31d2f1e4ee90aa5105186f24fb7e4
Carlos Mauricio Peñaranda Lino <ingmauriciopl@gmail.com>
2018-05-27 17:07:26 -0400

    second

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 
 var app = require('./app');   //para usar express desde el fichero app.js
 
-
-
 //socket
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(3800, function () {
+server.listen(process.env.PORT || 3800, function () {
     console.log("inicio del servidor node en el puerto 3800");
   });
+  
+
+
   
