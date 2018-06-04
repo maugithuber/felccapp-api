23eec2321dfbd5b587bf7d4e0f7dc1dc0119aff5
Carlos Mauricio Peñaranda Lino <ingmauriciopl@gmail.com>
2018-06-03 22:55:36 -0400

    asd

--- services/database.js
+++ services/database.js
@@ -1,17 +1,18 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-//    host: '5.79.67.193',   
-//    user: 'mauricio',      
-//    password: '214049973',
-//    database: 'felccapp',
-//    port: 3306
+   host: '5.79.67.193',   
+   user: 'mauricio',      
+   password: '214049973',
+   database: 'felccapp',
+   port: 3306
 
 
-host: 'localhost',   
-user: 'root',      
-password: '',
-database: 'felccapp',
-port: 3306
+// host: 'localhost',   
+// user: 'root',      
+// password: '',
+// database: 'felccapp',
+// port: 3306
+
 });
 
 connection.connect(function(error){
