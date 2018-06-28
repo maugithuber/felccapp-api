016e621dca6e10d67126e55d9db5536bebcace97
Carlos Mauricio Peñaranda Lino <ingmauriciopl@gmail.com>
2018-06-28 17:59:20 -0400

    asd

--- routes/alert.js
+++ routes/alert.js
@@ -10,7 +10,7 @@ api.get('/get-alerts' ,AlertController.getAlerts);
 api.get('/get-image-alert/:alertId', AlertController.getImageAlert);
 
 api.get('/get-robo' ,AlertController.getRobo);
-api.get('/get-vioalcion' ,AlertController.getViolacion);
+api.get('/get-violacion' ,AlertController.getViolacion);
 api.get('/get-violencia' ,AlertController.getViolencia);
 
 
